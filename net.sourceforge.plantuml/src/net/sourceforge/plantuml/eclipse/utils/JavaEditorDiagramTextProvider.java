fc0f376a1a930f1a8f79cb25db01900e055521bb
Hallvard Traetteberg <hal@idi.ntnu.no>
2011-01-12 14:43:47 +0100

    Fixed bug in automatic generation of class diagram from Java source.

--- net.sourceforge.plantuml/src/net/sourceforge/plantuml/eclipse/utils/JavaEditorDiagramTextProvider.java
+++ net.sourceforge.plantuml/src/net/sourceforge/plantuml/eclipse/utils/JavaEditorDiagramTextProvider.java
@@ -1,5 +1,7 @@
 package net.sourceforge.plantuml.eclipse.utils;
 
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
@@ -31,57 +33,119 @@ public class JavaEditorDiagramTextProvider extends AbstractDiagramTextProvider {
 		}
 		IPath path = ((IFileEditorInput) editorInput).getFile().getFullPath();
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0));
-		//		try {
-		//			if (project == null || (! project.isNatureEnabled("org.eclipse.jdt.core.javanature"))) {
-		//				return null;
-		//			}
-		//		} catch (CoreException e) {
-		//			return null;
-		//		}
-		//		IJavaProject javaProject = JavaCore.create(project);
-		//		IDocument document = ((ITextEditor) editorPart).getDocumentProvider().getDocument(editorInput);
 		ICompilationUnit compUnit = JavaCore.createCompilationUnitFrom(project.getFile(path.removeFirstSegments(1)));
 		StringBuilder result = new StringBuilder();
 		try {
 			compUnit.open(new NullProgressMonitor());
 			for (IType type: compUnit.getTypes()) {
-				result.append("class ");
-				result.append(type.getElementName());
-				result.append("{\n");
-				for (IField field : type.getFields()) {
-					result.append("\t");
-					result.append(getModifier(field));
-					result.append(getSignature(field.getTypeSignature()));
-					result.append(" ");
-					result.append(field.getElementName());
-					result.append("\n");
-				}
-				for (IMethod method : type.getMethods()) {
-					result.append("\t");
-					result.append(getModifier(method));
-					result.append(method.getElementName());
-					result.append("(");
-					String[] parameterTypes = method.getParameterTypes();
-					String[] parameterNames = null;
-					parameterNames = method.getParameterNames();
-					for (int i = 0; i < method.getNumberOfParameters(); i++) {
-						if (result.charAt(result.length() - 1) != '(') {
-							result.append(", ");
-						}
-						result.append(getSignature(parameterTypes[i]));
-						if (parameterNames != null) {
-							result.append(" ");
-							result.append(parameterNames[i]);
-						}
-					}
-					result.append(")\n");
-				}
-				result.append("}\n");
+				generateForType(type, result, GEN_MEMBERS | GEN_MODIFIERS | GEN_SUPERCLASS | GEN_INTERFACES, null);
 			}
 		} catch (JavaModelException e) {
 			System.err.println(e);
 		}
 		return (result.length() > 0 ? result.toString() : null);
+	}
+
+	private static int GEN_MODIFIERS = 1<<0, GEN_MEMBERS = 1<<1, GEN_SUPERCLASS = 1<<2, GEN_INTERFACES = 1<<3, GEN_ASSOCIATIONS = 1<<4;
+	
+	private void generateForType(IType type, StringBuilder result, int genFlags, List<IType> allTypes) {
+		result.append("class ");
+		result.append(type.getElementName());
+		result.append(" {\n");
+		try {
+			StringBuilder body = new StringBuilder();
+			if ((genFlags & GEN_MEMBERS) > 0) {
+				for (IField field : type.getFields()) {
+					String fieldTypeSignature = getSignature(field.getTypeSignature());
+					if ((genFlags & GEN_ASSOCIATIONS) > 0) {
+						generateRelatedClass(type, field.getTypeSignature(), "-->", body, null, field.getElementName(), (isMulti(fieldTypeSignature) ? "*" : "1"));
+					} else {
+						body.append("\t");
+						if ((genFlags & GEN_MODIFIERS) > 0) {
+							body.append(getModifier(field));
+						}
+						body.append(fieldTypeSignature);
+						body.append(" ");
+						body.append(field.getElementName());
+						body.append("\n");
+					}
+				}
+				for (IMethod method : type.getMethods()) {
+					body.append("\t");
+					if ((genFlags & GEN_MODIFIERS) > 0) {
+						body.append(getModifier(method));
+					}
+					body.append(method.getElementName());
+					body.append("(");
+					String[] parameterTypes = method.getParameterTypes();
+					String[] parameterNames = null;
+					parameterNames = method.getParameterNames();
+					for (int i = 0; i < method.getNumberOfParameters(); i++) {
+						if (body.charAt(body.length() - 1) != '(') {
+							body.append(", ");
+						}
+						body.append(getSignature(parameterTypes[i]));
+						if (parameterNames != null) {
+							body.append(" ");
+							body.append(parameterNames[i]);
+						}
+					}
+					body.append(")\n");
+				}
+			}
+			result.append(body);
+		} catch (JavaModelException e) {
+		}
+		result.append("}\n");
+		try {
+			if ((genFlags & GEN_SUPERCLASS) > 0) {
+				generateRelatedClass(type, type.getSuperclassTypeSignature(), "<|--", result);
+			}
+			if ((genFlags & GEN_INTERFACES) > 0) {
+				String[] interfaceSignatures = type.getSuperInterfaceTypeSignatures();
+				for (int i = 0; i < interfaceSignatures.length; i++) {
+					generateRelatedClass(type, interfaceSignatures[i], "<|..", result);
+				}
+			}
+		} catch (JavaModelException e) {
+		}
+	}
+
+	private boolean isMulti(String typeSignature) {
+		if (typeSignature.contains("[]")) {
+			return true;
+		}
+		if (typeSignature.contains("List<")) {
+			return true;
+		}
+		return false;
+	}
+
+	private void generateRelatedClass(IType type, String superClassSignature, String relation, StringBuilder result) {
+		generateRelatedClass(type, superClassSignature, relation, result, null, null, null);
+	}
+	private void generateRelatedClass(IType type, String superClassSignature, String relation, StringBuilder result, String start, String middle, String end) {
+		if (superClassSignature != null) {
+			String superClassName = getSignature(superClassSignature);
+			if (! superClassName.equals("Object")) {
+				result.append("class ");
+				result.append(superClassName);
+				result.append(" {\n}\n");
+				result.append(superClassName);
+				if (start != null) {
+					result.append(" \"" + start + "\" ");
+				}
+				result.append(" " + relation + " ");
+				if (end != null) {
+					result.append(" \"" + end + "\" ");
+				}
+				result.append(type.getElementName());
+				if (middle != null) {
+					result.append(" : " + middle);
+				}
+				result.append("\n");
+			}
+		}
 	}
 
 	private String getSignature(String signature) {
